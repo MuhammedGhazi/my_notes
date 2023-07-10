6484046c62e6bb4893a6c8414817f3fb343f8c44
MuhammedGhazi <muhamedghazi79@gmail.com>
2023-07-11 00:45:03 +0300

    create note item

--- lib/features/home/presentaion/views/notes_view.dart
+++ lib/features/home/presentaion/views/notes_view.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:my_notes/features/home/presentaion/views/widgets/notes_View_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return const Scaffold(
+      body: NotesViewBody(),
     );
   }
 }
---
+++ lib/features/home/presentaion/views/widgets/notes_View_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:my_notes/features/home/presentaion/views/widgets/custom_app_bar.dart';
+
+import 'note_item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: const [
+          SizedBox(height: 50),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
