5574d75d51b9e31057d160aaefb1100a3a7c2061
MuhammedGhazi <muhamedghazi79@gmail.com>
2023-07-14 15:48:29 +0300

    take input and validate data

--- lib/features/home/presentaion/views/widgets/custom_button.dart
+++ lib/features/home/presentaion/views/widgets/custom_button.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:my_notes/core/constantes.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
-      width: MediaQuery.of(context).size.width,
-      height: 40,
-      child: const Center(
-          child: Text(
-        "Add note",
-        style: TextStyle(color: Colors.black, fontSize: 16),
-      )),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
+        width: MediaQuery.of(context).size.width,
+        height: 40,
+        child: const Center(
+            child: Text(
+          "Add note",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        )),
+      ),
     );
   }
 }
--- lib/features/home/presentaion/views/widgets/custom_text_field.dart
+++ lib/features/home/presentaion/views/widgets/custom_text_field.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, required this.maxline});
+  const CustomTextField(
+      {super.key, required this.hint, required this.maxline, this.onSaved});
   final String hint;
   final int maxline;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+        onSaved: onSaved,
+        validator: (value) {
+          return (value != null && value.isNotEmpty
+              ? null
+              : 'Field is requied');
+        },
         maxLines: maxline,
         decoration: InputDecoration(
           hintText: hint,
