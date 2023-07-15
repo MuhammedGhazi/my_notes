3ba6587a3f20fef4b3fde551b32e60c31b16a5b4
MuhammedGhazi <muhamedghazi79@gmail.com>
2023-07-16 00:01:13 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_notes/features/home/presentaion/manage/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:my_notes/features/home/presentaion/manage/simple_bloc_observer.dart';
 import 'package:my_notes/features/home/presentaion/views/notes_view.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   runApp(const NotesApp());
 }
 
